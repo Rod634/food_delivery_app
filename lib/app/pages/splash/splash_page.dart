ec12c8b3b0481bed14f78de704995a95b9fcd6c7
rodrigo <rodrigocsilva63@gmail.com>
2023-02-01 16:56:52 -0300

    add size extension helper

--- lib/app/pages/splash/splash_page.dart
+++ lib/app/pages/splash/splash_page.dart
@@ -1,3 +1,4 @@
+import 'package:dw9_delivery_app/app/core/ui/helpers/size_extensions.dart';
 import 'package:dw9_delivery_app/app/core/ui/widgets/delivery_button.dart';
 import 'package:flutter/material.dart';
 
