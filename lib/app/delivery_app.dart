897eed2850ae3e6f7736168d28a8be7e041376f8
rodrigo <rodrigocsilva63@gmail.com>
2023-02-01 12:10:04 -0300

    add a splash page and our main route

---
+++ lib/app/delivery_app.dart
@@ -0,0 +1,16 @@
+import 'package:dw9_delivery_app/app/pages/splash/splash_page.dart';
+import 'package:flutter/material.dart';
+
+class DeliveryApp extends StatelessWidget {
+  const DeliveryApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Delivery App',
+      routes: {
+        '/': (context) => const SplashPage(),
+      },
+    );
+  }
+}
