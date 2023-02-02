86a86b22f50dea8ae03d3ed2e44da3ded07abdac
rodrigo <rodrigocsilva63@gmail.com>
2023-02-02 11:17:15 -0300

    create product repository and repository exception

---
+++ lib/app/repositories/products_repository_impl.dart
@@ -0,0 +1,27 @@
+import 'dart:developer';
+
+import 'package:dio/dio.dart';
+import 'package:dw9_delivery_app/app/models/product_model.dart';
+
+import '../core/Exceptions/repository_exception.dart';
+import '../core/restClient/custom_dio.dart';
+import './products_repository.dart';
+
+class ProductsRepositoryImpl implements ProductsRepository {
+  final CustomDio dio;
+
+  ProductsRepositoryImpl({
+    required this.dio,
+  });
+
+  @override
+  Future<List<ProductModel>> findAllProducts() async {
+    try {
+      final result = await dio.unAuth().get('/products');
+      return result.data.map((p) => ProductModel.fromMap(p)).toList();
+    } on DioError catch (e, s) {
+      log('Erro ao buscar produtos', error: e, stackTrace: s);
+      throw RepositoryException(message: 'Erro ao buscar produtos');
+    }
+  }
+}
