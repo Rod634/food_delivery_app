02f0ad2aad1ca50551e15e145177220b832340a7
rodrigo <rodrigocsilva63@gmail.com>
2023-02-02 14:15:43 -0300

    create a loader helper

--- lib/app/pages/home/widgets/delivery_product_tile.dart
+++ lib/app/pages/home/widgets/delivery_product_tile.dart
@@ -1,5 +1,4 @@
 import 'package:dw9_delivery_app/app/core/extensions/formatter_extension.dart';
-import 'package:dw9_delivery_app/app/core/ui/styles/app_styles.dart';
 import 'package:dw9_delivery_app/app/core/ui/styles/color_app.dart';
 import 'package:dw9_delivery_app/app/core/ui/styles/text_styles.dart';
 import 'package:dw9_delivery_app/app/models/product_model.dart';
@@ -32,7 +31,8 @@ class DeliveryProductTile extends StatelessWidget {
                   padding: const EdgeInsets.only(bottom: 8.0),
                   child: Text(
                     product.description,
-                    style: context.textStyles.textLight.copyWith(fontSize: 12),
+                    style:
+                        context.textStyles.textRegular.copyWith(fontSize: 12),
                   ),
                 ),
                 Padding(
