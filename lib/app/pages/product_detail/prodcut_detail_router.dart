50991c55f6bfd471953645d5d02aac942bbd2e27
rodrigo <rodrigocsilva63@gmail.com>
2023-02-03 19:47:35 -0300

    add increment_decrement logic, bloc state with cubit

--- lib/app/pages/product_detail/prodcut_detail_router.dart
+++ lib/app/pages/product_detail/prodcut_detail_router.dart
@@ -1,4 +1,5 @@
 import 'package:dw9_delivery_app/app/pages/product_detail/product_detail.dart';
+import 'package:dw9_delivery_app/app/pages/product_detail/product_detail_controller.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 
@@ -6,7 +7,12 @@ class ProdcutDetailRouter {
   ProdcutDetailRouter._();
 
   static Widget get page => MultiProvider(
-        providers: [Provider(create: (context) => Object())],
-        child: ProductDetail(),
+        providers: [Provider(create: (context) => ProductDetailController())],
+        builder: (context, child) {
+          final args = ModalRoute.of(context)!.settings.arguments
+              as Map<String, dynamic>;
+
+          return ProductDetail(product: args['product']);
+        },
       );
 }
--- lib/app/pages/product_detail/product_detail.dart
+++ lib/app/pages/product_detail/product_detail.dart
@@ -1,14 +1,30 @@
 import 'package:auto_size_text/auto_size_text.dart';
+import 'package:dw9_delivery_app/app/core/extensions/formatter_extension.dart';
 import 'package:dw9_delivery_app/app/core/ui/helpers/size_extensions.dart';
 import 'package:dw9_delivery_app/app/core/ui/styles/text_styles.dart';
 import 'package:dw9_delivery_app/app/core/ui/widgets/delivery_appbar.dart';
+import 'package:dw9_delivery_app/app/models/product_model.dart';
+import 'package:dw9_delivery_app/app/pages/product_detail/product_detail_controller.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 
+import '../../core/ui/base_state/base_state.dart';
 import '../../core/ui/widgets/delivery_increment_decrement_Button.dart';
 
-class ProductDetail extends StatelessWidget {
-  const ProductDetail({super.key});
+class ProductDetail extends StatefulWidget {
+  final ProductModel product;
 
+  const ProductDetail({
+    super.key,
+    required this.product,
+  });
+
+  @override
+  State<ProductDetail> createState() => _ProductDetailState();
+}
+
+class _ProductDetailState
+    extends BaseState<ProductDetail, ProductDetailController> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -19,10 +35,9 @@ class ProductDetail extends StatelessWidget {
           Container(
             width: context.screenWidth,
             height: context.percentHeight(.4),
-            decoration: const BoxDecoration(
+            decoration: BoxDecoration(
               image: DecorationImage(
-                image: NetworkImage(
-                    'https://assets.unileversolutions.com/recipes-v2/106684.jpg?imwidth=800'),
+                image: NetworkImage(widget.product.image),
                 fit: BoxFit.cover,
               ),
             ),
@@ -30,16 +45,15 @@ class ProductDetail extends StatelessWidget {
           Padding(
             padding: const EdgeInsets.all(10),
             child: Text(
-              'X-Burger',
+              widget.product.name,
               style: context.textStyles.textExtraBold.copyWith(fontSize: 22),
             ),
           ),
-          const Expanded(
+          Expanded(
             child: Padding(
               padding: EdgeInsets.symmetric(horizontal: 10),
               child: SingleChildScrollView(
-                child: Text(
-                    'Lanche acompanha pão, hambúguer, mussarela, alface, tomate e maionese'),
+                child: Text(widget.product.description),
               ),
             ),
           ),
@@ -51,37 +65,54 @@ class ProductDetail extends StatelessWidget {
                 width: context.percentWidth(.5),
                 height: 68,
                 padding: const EdgeInsets.all(8),
-                child: const DeliveryIncrementDecrementButton(),
+                child: BlocBuilder<ProductDetailController, int>(
+                  builder: (context, amount) {
+                    return DeliveryIncrementDecrementButton(
+                      amount: amount,
+                      decrementTap: () {
+                        controller.decrement();
+                      },
+                      incrementTap: () {
+                        controller.increment();
+                      },
+                    );
+                  },
+                ),
               ),
               Container(
                 width: context.percentWidth(.5),
                 padding: const EdgeInsets.all(8),
                 height: 68,
-                child: ElevatedButton(
-                  onPressed: () {},
-                  child: Row(
-                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                    children: [
-                      Text(
-                        'Adicionar',
-                        style: context.textStyles.textExtraBold
-                            .copyWith(fontSize: 13),
+                child: BlocBuilder<ProductDetailController, int>(
+                  builder: (context, amount) {
+                    return ElevatedButton(
+                      onPressed: () {},
+                      child: Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        children: [
+                          Text(
+                            'Adicionar',
+                            style: context.textStyles.textExtraBold
+                                .copyWith(fontSize: 13),
+                          ),
+                          const SizedBox(
+                            width: 10,
+                          ),
+                          Expanded(
+                            child: AutoSizeText(
+                              (widget.product.price * amount).currencyPTBR,
+                              maxFontSize: 13,
+                              minFontSize: 5,
+                              maxLines: 1,
+                              textAlign: TextAlign.center,
+                              style: context.textStyles.textExtraBold
+                                  .copyWith(fontSize: 13),
+                            ),
+                          ),
+                        ],
                       ),
-                      const SizedBox(
-                        width: 10,
-                      ),
-                      Expanded(
-                        child: AutoSizeText(
-                          r'R$ 6.99',
-                          maxFontSize: 13,
-                          minFontSize: 5,
-                          maxLines: 1,
-                          style: context.textStyles.textExtraBold
-                              .copyWith(fontSize: 13),
-                        ),
-                      ),
-                    ],
-                  ),
+                    );
+                  },
                 ),
               ),
             ],
